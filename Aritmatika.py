9db5618437136aacddd89e26bccfc7d17f619eaa
Azakana <danielsaputradivo@gmail.com>
2024-01-16 20:38:25 +0700

    menambahkan file Aritmatika.py ke repo

---
+++ Aritmatika.py
@@ -0,0 +1,34 @@
+X = 10
+Y = 5
+#  +
+hasil = X + Y
+print(X, "+", Y, "=", hasil)
+
+# -
+
+hasil = X - Y
+print(X, "-", Y, "=", hasil)
+
+# / 
+
+hasil = X / Y
+print(X, "/", Y, "=", hasil)
+
+#  X
+hasil = X * Y
+print(X, "X", Y, "=", hasil)
+
+# Eksponen 
+hasil = X ** Y
+print(X, "**", Y, "=", hasil)
+
+# Modulus
+hasil = X % Y
+print(X, "%", Y, "=", hasil)
+
+# Flow Division
+hasil = X // Y
+print(X, "//", Y, "=", hasil)
+
+# prioritas operasi
+#(), eksponen, perkalian, pertambahan
