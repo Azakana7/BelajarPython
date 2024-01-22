f3ad3ee6f4ecc51a7a11e9b0c49536a827ab0e0e
Azakana <danielsaputradivo@gmail.com>
2024-01-22 22:46:09 +0700

    Menambah file

---
+++ 4Input.py
@@ -0,0 +1,18 @@
+#a = input("Masukkan angka pertama: ")
+#b = input("Masukkan angka kedua: ")
+#c = int(a) + int(b)
+
+#print("Hasilnya adalah: " + str(c))
+
+# menampilkan hasil depo ke layar terminal
+#saldo_awal = input("Berapa saldo pertamamu: ")
+#deposit = input("Mau depo berapa?: ")
+
+#saldo_akhir = int(saldo_awal) + int(deposit)
+#print(saldo_akhir)
+
+
+
+nama_saya = "BayuS Segara"
+
+print(len(nama_saya))
