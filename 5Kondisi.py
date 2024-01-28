1dcee6bc201d2d40258b7c93a512e15b50576965
Azakana <danielsaputradivo@gmail.com>
2024-01-28 13:35:06 +0700

    tes

--- 5Kondisi.py
+++ 5Kondisi.py
@@ -1,4 +1,4 @@
-usia = 20
+usia = 31
 
 # == sama dengan
 # > lebih dari
@@ -7,7 +7,19 @@ usia = 20
 # >= lebih dari sama dengan 
 # <= kurang dari sama dengan
 
-if usia == 20:
-    print("Hai anak muda")
+if usia == 0 :
+    print("Belum lahir")
+elif usia >= 0 and usia <= 3:
+    print("Balita")
+elif usia >= 3 and usia <= 10:
+    print("Anak TK")
+elif usia >= 10 and usia <= 15:
+    print('Remaja')
+elif usia >= 15 and usia <= 20:
+    print("Dewasa awal")
+elif usia >= 20 and usia <= 30:
+    print('Dewasa akhir')
+elif usia >=30 and usia <= 50:
+    print("Fosil")
 else:
-    print("Dasar tua")
+    print("pengecualian")
